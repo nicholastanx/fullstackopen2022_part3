9694ab0cbfd2fb6386a65c09391b7a2451776885
nicholas.tan <nicholas.tan@acronis.com>
2022-03-15 11:46:07 +0800

    uibuild

--- index.js
+++ index.js
@@ -127,5 +127,5 @@ app.use(unknownEndpoint)
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}!`)
+  console.log(`Backend server is running on ${PORT}!`)
 })
